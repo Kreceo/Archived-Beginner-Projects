beff841dacfeb7b4c785c6c1694cad2a4d92adb8
Darrell Lane <darrell-lane@Darrells-MacBook-Pro.local>
2019-02-18 22:13:01 +0000

    additional step to the ball moving around the cube

--- Javascript-Games/Game-1/js/index.js
+++ Javascript-Games/Game-1/js/index.js
@@ -13,7 +13,7 @@ var rightPressed = false;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc( x, y, ballRadius, 0, Math.PI*2 );
+    ctx.arc( x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
