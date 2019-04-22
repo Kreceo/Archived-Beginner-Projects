6e54c4054508af05bc3183693a06f51c75a0f5bb
Darrell Lane <darrell-lane@Darrells-MacBook-Pro.local>
2019-04-22 10:44:16 +0100

    Udemy course training files and responses

---
+++ PHP-Training-Udemy/My_Course_Files/variables.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Variables</title>
+</head>
+<body>
+    
+<?php 
+
+    $myVariable = "This is a variable";
+
+    // Boolean vaiable
+
+    $loggedIn = true;
+
+    //Integer 
+
+    $myAge = 28;
+
+    //
+
+    $totalPrice = 150.00;
+
+    //String
+
+    $fullName = "Darrell Lane";
+
+    // Display Variables on screen
+
+    echo "Hello, My name is $fullName and I am $myAge";
+    
+?>
+
+
+</body>
+</html>
